4818461d3f94679e12c1cab3378e2c6c8b6bcf35
Sayan Sarkar <72155617+ky13-troj@users.noreply.github.com>
2024-06-20 04:33:15 +0530

    sd

--- autoGIT.py
+++ autoGIT.py
@@ -1,18 +1,30 @@
+import os
+import argparse
 from git_uploader import GitHubUploader
 from files import FileManager
 
+def upload_directory(uploader, repo_name, directory, commit_msg):
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            uploader.upload_file(repo_name, file_path, commit_msg)
+
 def main():
+    parser = argparse.ArgumentParser(description='Auto upload to GitHub.')
+    parser.add_argument('-p', '--path', type=str, default='.', help='Path to upload')
+    args = parser.parse_args()
+
     file_manager = FileManager()
-    files = file_manager.list_files()
-    selected_files = file_manager.select_files(files)
     uploader = GitHubUploader()
     repos = uploader.list_repos()
     selected_repo = uploader.select_repo(repos)
     print(f"You selected: {selected_repo['name']}")
-    
+
     commit_msg = input("Enter commit message: ")
-    for file in selected_files:
-        uploader.upload_file(selected_repo['name'], file, commit_msg)
+    if os.path.isdir(args.path):
+        upload_directory(uploader, selected_repo['name'], args.path, commit_msg)
+    else:
+        uploader.upload_file(selected_repo['name'], args.path, commit_msg)
 
 if __name__ == "__main__":
     main()
